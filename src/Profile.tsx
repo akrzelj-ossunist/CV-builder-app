2ac084772d65d7040d853a6183ad2c8fd5dc5999
akrzelj-ossunist <krzelj99antonio@gmail.com>
2023-06-01 03:27:08 +0200

    page to pdf

--- src/Profile.tsx
+++ src/Profile.tsx
@@ -1,5 +1,14 @@
+import { Preview, print } from "react-html2pdf";
+
 const Profile: React.FC<{ info: any }> = ({ info }) => {
-  return <>{JSON.stringify(info)}</>;
+  return (
+    <>
+      <Preview id={"jsx-template"}>
+        <div id="content">{JSON.stringify(info)}</div>
+      </Preview>
+      <button onClick={() => print("a", "jsx-template")}> print</button>
+    </>
+  );
 };
 
 export default Profile;
